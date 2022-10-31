85b8abc7f21700ff4a09ed4b079bbe3424f70c2b
Yong Liang <yoliang@ucsd.edu>
2022-10-30 17:26:35 -0700

    update readme, change url in expose.js

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Lab 5 - Starter
+- Contributor: Yong Liang
+- Link to expose.html: [https://chrisliang12.github.io/Lab5_Starter/expose.html](https://chrisliang12.github.io/Lab5_Starter/expose.html)
+- Link to explore.html: [https://chrisliang12.github.io/Lab5_Starter/explore.html](https://chrisliang12.github.io/Lab5_Starter/explore.html)
 - Github Action Repo Link: [https://github.com/chrisliang12/introduction-to-github](https://github.com/chrisliang12/introduction-to-github)
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -22,13 +22,13 @@ function init() {
   volume.addEventListener('change', () => {
     //console.log(typeof(volume.value));
     if (volume.value == 0) {
-      icon.setAttribute('src', "/assets/icons/volume-level-0.svg");
+      icon.setAttribute('src', "assets/icons/volume-level-0.svg");
     } else if (volume.value >= 1 && volume.value < 33) {
-      icon.setAttribute('src', "/assets/icons/volume-level-1.svg");
+      icon.setAttribute('src', "assets/icons/volume-level-1.svg");
     } else if (volume.value >=33 && volume.value < 67) {
-      icon.setAttribute('src', "/assets/icons/volume-level-2.svg");
+      icon.setAttribute('src', "assets/icons/volume-level-2.svg");
     } else {
-      icon.setAttribute('src', "/assets/icons/volume-level-3.svg");
+      icon.setAttribute('src', "assets/icons/volume-level-3.svg");
     }
   });
 
